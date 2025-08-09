b7884da0088c6c61eb86c61afaad9d1290a33c6c
John Xie <johnxie531@gmail.com>
2025-08-09 17:59:41 +0800

    fixed default value for timespan

--- Webapp/Frontend/src/components/TimeSpanSelector.jsx
+++ Webapp/Frontend/src/components/TimeSpanSelector.jsx
@@ -2,7 +2,7 @@ import '../App.css'
 
 function TimeSpanSelector({timeSpan, setTimeSpan}){
     return(
-        <select className='select' onChange={(e) => setTimeSpan(e.target.value)} timeSpan={timeSpan}>
+        <select defaultValue={-365} className='select' onChange={(e) => setTimeSpan(e.target.value)}>
           <option value={-7}>Past 7 Days</option>
           <option value={-30}>Past 30 Days</option>
           <option value={-90}>Past 90 Days</option>
